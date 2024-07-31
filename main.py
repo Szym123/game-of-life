a3bdd3adb2d01ca4a084809de6fe210a5db11061
szymon <szymon@DESKTOP-GN952CV.>
2024-07-31 23:04:27 +0200

    Add new function

--- main.py
+++ main.py
@@ -3,39 +3,69 @@ from signal import signal,SIGWINCH
 from time import sleep
 
 import random
+import time
 
 stdscr=initscr()
+Board=[]
+NewBoard=[]
+
+####################################################################
+
+def openFile(Name):
+    Tab=[]
+
+    try:
+        File=open(Name,"r")
+        # open a file in read mode
+
+        while True:
+            Line=File.readline()
+            if not Line:
+                break
+            Tab.append(Line.split())
+        # read file line by line
+
+        return Tab
+        # return table of line
+
+    except:
+        return -1
+
+####################################################################
+
+def settingParsing(Board):
+    pass
 
 ####################################################################
 
 def generateBord():
-    Board=[]
 
     Rows,Cols=stdscr.getmaxyx()
     for _ in range(Rows-2):
         Board.append([" " for _ in range(Cols-2)])
+    # Generate matrix of dead cell with specific size
 
-    return Board
+    for _ in range(Rows-2):
+        NewBoard.append([" " for _ in range(Cols-2)])
 
 ####################################################################
 
-def generateStartConf(Board,Rows,Cols,Number):
+def generateStartConf(Rows,Cols,Number):
     for Iter in range(Number):
         y=random.randrange(Rows-2)
         x=random.randrange(Cols-2)
+        # Generate radom position of living cell
 
         Board[y][x]="O"
 
-    return Board
-
 ####################################################################
 
-def drawBoard(Board,Cols):
+def drawBoard(Cols):
     Iter=1
     for Line in Board:
         String="".join(Line)
         stdscr.addnstr(Iter,1,String,Cols-1)
-        Iter+=1
+        Iter+=1 
 
 ####################################################################
 
@@ -46,14 +76,10 @@ def drawStdScreen():
     stdscr.clear()
 
     stdscr.border()
-    stdscr.addnstr(0,1," GAME OF LIFE ",Cols-1)
+    stdscr.addnstr(0,1," Game of Life ",Cols-1)
     # Draw border with name of program
 
-    Board=generateBord()
-
-    Board=generateStartConf(Board,Rows,Cols,100)
-
-    drawBoard(Board,Cols)
+    drawBoard(Cols)
 
     stdscr.refresh()
 
@@ -67,6 +93,51 @@ def resizeHandler(Signum,Frame):
 
 ####################################################################
 
+def sumOfNeighbors(PosY,PosX):
+    Sum=0
+
+    IterY=PosY-1
+    while IterY<=(PosY+1):
+        IterX=PosX-1
+        while IterX<=(PosX+1):
+            try:
+                if (IterX!=PosX or IterY!=PosY) and Board[IterY][IterX]=="O":
+                    Sum+=1
+            except:
+                pass
+            IterX+=1
+        IterY+=1
+
+    return Sum
+
+####################################################################
+
+def calculateTransformation(Rows,Cols):
+    IterY=0
+    while IterY<(Rows-2):
+        IterX=0
+        while IterX<(Cols-2):
+            Sum=sumOfNeighbors(IterY,IterX)
+
+            if Sum==3:
+                NewBoard[IterY][IterX]="O"
+
+            if not(Sum in [2,3]):
+                NewBoard[IterY][IterX]=" "
+
+            IterX+=1
+        IterY+=1
+
+####################################################################
+
+def copyArray():
+    Iter=0
+    for Item in NewBoard:
+        Board[Iter]=Item
+        Iter+=1
+
+####################################################################
+
 signal(SIGWINCH,resizeHandler)
 
 ####################################################################    
@@ -75,11 +146,25 @@ def main():
 
     initscr()
 
+    Rows,Cols=stdscr.getmaxyx()
+    generateBord()
+    generateStartConf(Rows,Cols,400)
+
     try:
         drawStdScreen()
-        while 1:
-            # print stuff with curses
-            sleep(1)
+        Iter=1
+        while True:
+            time.sleep(0.1)
+
+            calculateTransformation(Rows,Cols)
+
+            copyArray()
+
+            drawBoard(Cols)
+
+            stdscr.addnstr(Rows-1,1," Step: "+str(Iter),Cols-1)
+            Iter+=1
+            stdscr.refresh()
     except (KeyboardInterrupt, SystemExit):
         pass
     except Exception as e:
@@ -89,3 +174,5 @@ def main():
 
 ####################################################################
 
+if __name__=="__main__":
+	main()
